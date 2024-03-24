bda305c76eefafc78ffdb4d6497bac356ffeb810
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-03-24 23:13:42 +0300

    Add files via upload

---
+++ CampaingController.java
@@ -0,0 +1,33 @@
+package com.patika.kredinbizdeservice.controller;
+
+import com.patika.kredinbizdeservice.model.Campaign;
+import com.patika.kredinbizdeservice.service.CampaignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/campaigns")
+public class CampaignController {
+
+    private final CampaignService campaignService;
+
+    @Autowired
+    public CampaignController(CampaignService campaignService) {
+        this.campaignService = campaignService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Campaign>> getAllCampaigns() {
+        List<Campaign> campaigns = campaignService.getAllCampaigns();
+        Collections.sort(campaigns, (c1, c2) -> c2.getCreateDate().compareTo(c1.getCreateDate())); // En güncelden eskiye sıralama
+        return new ResponseEntity<>(campaigns, HttpStatus.OK);
+    }
+}
