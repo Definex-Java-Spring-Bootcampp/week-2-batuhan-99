cce5ff2a0d6c269d80676b1eff46f135a614d1e8
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-03-24 23:16:13 +0300

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,3 +1,14 @@
+package com.patika.kredinbizdeservice.controller;
+
+import com.patika.kredinbizdeservice.model.Application;
+import com.patika.kredinbizdeservice.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 public class UserController {
 
